ae579b8d2dc4c0f84cd146e70fd40624cbada15a
someoneelsebutmyself <someoneelsebutmyself@e50dc7d2-40fc-11de-9805-9778bc541ee5>
2009-05-15 12:41:03 +0000

    Initial work on getting a clean surface to draw the game on.

---
+++ game/src/innerGame/paintSurface.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package innerGame;
+import java.awt.Color;
+import javax.swing.*;
+/**
+ * @author aj
+ *
+ */
+public class paintSurface {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame window = new JFrame("DeWitt Awesome Game");
+		window.setSize(800, 600);
+		window.setBackground(Color.white);
+		window.setVisible(true);
+	}
+
+}
