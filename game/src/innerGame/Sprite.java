0db107b72b1b148581e760692a1c053e56e43201
someoneelsebutmyself <someoneelsebutmyself@e50dc7d2-40fc-11de-9805-9778bc541ee5>
2009-05-16 05:55:32 +0000

    Coding around 2AM...Donuts now fill the screen. Wow.

---
+++ game/src/innerGame/Sprite.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package innerGame;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+import sun.security.util.Debug;
+
+/**
+ * @author aj
+ *
+ */
+public class Sprite extends Component {
+
+	/**
+	 * 
+	 */
+	private BufferedImage image;
+	private Point currentPosition = new Point(0,0);
+	private Boolean anchored = false;
+	private static final long serialVersionUID = -8777748656612457715L;
+    
+	public Sprite(String graphicFile, Point initialPosition, Boolean isAnchored)
+	{
+		try
+		{
+			File file = new File(graphicFile);
+			image = ImageIO.read(file);
+			currentPosition = initialPosition;
+			anchored = isAnchored;
+			this.repaint();
+		}
+		catch (Exception ex)
+		{
+			
+		}
+	}
+	public void paint(Graphics g) {
+		System.out.println("I was redrawn!");
+        g.drawImage(image, currentPosition.x, currentPosition.y, null); // see javadoc for more info on the parameters
+    }
+}
--- game/src/innerGame/paintSurface.java
+++ game/src/innerGame/paintSurface.java
@@ -2,7 +2,11 @@
  * 
  */
 package innerGame;
+import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.Point;
+
 import javax.swing.*;
 /**
  * @author aj
@@ -16,9 +20,12 @@ public class paintSurface {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JFrame window = new JFrame("DeWitt Awesome Game");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setSize(800, 600);
-		window.setBackground(Color.white);
+		window.getContentPane().setBackground(Color.white);
+		window.add(new Sprite("C:\\Users\\aj\\Documents\\Downloads\\donuts.jpg", new Point(0,0), true));
 		window.setVisible(true);
+		window.repaint();
 	}
 
 }
